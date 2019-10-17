d81a642338f7be05682925cb1b150a70c639c886
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-17 16:37:56 -0400

    Fix Thymeleaf links syntax

--- src/main/java/com/danielgkneto/mcjavabc/jobposting/HomeController.java
+++ src/main/java/com/danielgkneto/mcjavabc/jobposting/HomeController.java
@@ -28,9 +28,10 @@ public class HomeController {
         return "index";
     }
 
-    @RequestMapping("/docs")
-    public String docs(Model model){
-        return "docs";
+    @RequestMapping("/myjobs")
+    public String userJobList(Model model){
+        model.addAttribute("jobs", jobRepository.findAll());
+        return "myjobs";
     }
 
     @GetMapping("/add")
@@ -40,7 +41,7 @@ public class HomeController {
     }
 
     @PostMapping("/processjob")
-    public String saveJob(@ModelAttribute Job job /*, @RequestParam(name = "postedDate")
+    public String saveJob(@ModelAttribute Job job, Principal principal /*, @RequestParam(name = "postedDate")
             String postedDate */){
 /*
         String pattern = "yyyy-MM-dd";
@@ -57,6 +58,7 @@ public class HomeController {
         }
 */
 
+        job.setUser(userRepository.findByUsername(principal.getName()));
         jobRepository.save(job);
         return "redirect:/";
     }
@@ -90,7 +92,7 @@ public class HomeController {
         model.addAttribute("jobs", jobs);
         return "index";
     }
-/*
+
     @GetMapping("/register")
     public String showRegistrationPage(Model model) {
         model.addAttribute("user", new User());
@@ -110,129 +112,15 @@ public class HomeController {
         return "redirect:/";
     }
 
-    @RequestMapping("/secure")
+/*    @RequestMapping("/secure")
     public String secure(Principal principal, Model model) {
         String username = principal.getName();
         model.addAttribute("user", userRepository.findByUsername(username));
         return "secure";
-    }
-
-    @RequestMapping("/")
-    public String carList(Model model){
-        model.addAttribute("cars", carRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "index";
-    }
+    }*/
 
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
-
-    @RequestMapping("categories")
-    public String categoryList(Model model){
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "categories";
-    }
-
-*/
-/*    @RequestMapping("/docs")
-    public String docs(Model model){
-        return "docs";
-    }*//*
-
-
-    @GetMapping("/addcar")
-    public String addCar(Model model){
-        model.addAttribute("car", new Car());
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "carform";
-    }
-
-    @GetMapping("/addcategory")
-    public String addCategory(Model model){
-        model.addAttribute("category", new Category());
-        return "categoryform";
-    }
-
-    @PostMapping("/processcar")
-    public String saveCar(@ModelAttribute Car car, Principal principal){
-        car.setUser(userRepository.findByUsername(principal.getName()));
-        carRepository.save(car);
-        return "redirect:/";
-    }
-
-    @PostMapping("/processcategory")
-    public String saveCategory(@ModelAttribute Category category){
-        categoryRepository.save(category);
-        return "redirect:/categories";
-    }
-
-    @RequestMapping("/detail/{id}")
-    public String showCar(@PathVariable("id") long id, Model model){
-        model.addAttribute("car", carRepository.findById(id).get());
-        return "showcar";
-    }
-
-    @RequestMapping("/update/{id}")
-    public String updateCar(@PathVariable("id") long id, Model model){
-        model.addAttribute("car", carRepository.findById(id).get());
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "carform";
-    }
-
-    @RequestMapping("/delete/{id}")
-    public String deleteCar(@PathVariable("id") long id){
-        carRepository.deleteById(id);
-        return "redirect:/";
-    }
-
-    @RequestMapping("/categoryupdate/{id}")
-    public String updateCategory(@PathVariable("id") long id, Model model){
-        model.addAttribute("category", categoryRepository.findById(id).get());
-        return "categoryform";
-    }
-
-    @RequestMapping("/categorydelete/{id}")
-    public String deleteCategory(@PathVariable("id") long id, Model model){
-        try {
-            categoryRepository.deleteById(id);
-            return "redirect:/categories";
-        }
-        catch (Exception e) {
-            model.addAttribute("error_message", "You can't delete this category because there is at least one car registered under it.");
-            model.addAttribute("return_link", "/categories");
-            return "error";
-        }
-    }
-
-    @RequestMapping("/pickcategory/{chosencategory}")
-    public String pickCategory(@PathVariable("chosencategory") long chosenCategory, Model model){
-        Category c = categoryRepository.findById(chosenCategory).get();
-        model.addAttribute("cars", carRepository.findByCategory(c));
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "index";
-    }
-
-    @PostMapping("/processsearch")
-    public String searchResult(Model model,@RequestParam(name="search") String search) {
-        String[] keywords = search.split(" ");
-        Set<Car> cars = new HashSet<Car>();
-
-        for (int i = 0; i < keywords.length; i++) {
-            cars.addAll(carRepository.findByManufacturerContainingIgnoreCaseOrModelContainingIgnoreCase(keywords[i], keywords[i]));
-        }
-        model.addAttribute("cars", cars);
-        model.addAttribute("categories", categoryRepository.findAll());
-        return "index";
-    }
-
-    @GetMapping("/userprofile")
-    public String showProfile(Principal principal, Model model) {
-        String username = principal.getName();
-        model.addAttribute("user", userRepository.findByUsername(username));
-        return "userprofile";
-    }
-
-*/
 }
